d6cb8852b34acc26e925d2bf9e34a11c9a0f8a16
Martin K <martin.kutt@khk.ee>
2020-12-18 08:36:17 +0200

    Suurendatud väljund

--- src/test.java
+++ src/test.java
@@ -1,6 +1,7 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Alakabaar!");
-        System.out.println("Ruun maan ruuun!");
+        System.out.println("Täna kasutasime git-i!");
+        System.out.println("Ei ma kakkan teie järgi seda tegema!");
     }
 }
