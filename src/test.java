574453303ddab2f6a791c17db33522049aabe36b
Martin K <martin.kutt@khk.ee>
2021-01-04 15:16:31 +0200

    if-else-if ülesanne 2.2 Kandideerimine

--- src/test.java
+++ src/test.java
@@ -1,6 +1,6 @@
 /*
-If and else Pilvede ülesanne
-ülesanne 2.1
+Kandideerimine
+ülesanne 2.2
  */
 
 import java.util.Scanner;
@@ -8,12 +8,19 @@ import java.util.Scanner;
 public class test {
     public static void main(String[] args) {
         Scanner sisend = new Scanner(System.in);
-        System.out.println("Palun sisestage pilvede kõrgus: ");
-        double pilvedeKõrgus = sisend.nextDouble();
-        if (pilvedeKõrgus > 6.0) {
-            System.out.println("Need on ülemised pilved.");
-        } else {
-            System.out.println("Need ei ole ülemised pilved.");
+
+        System.out.println("Palun sisestage punktide summa: ");
+        // Kasutaja poolt sisend
+        double punktid = sisend.nextDouble();
+        // Kontroll
+        if (punktid >= 0 && punktid < 66) {
+            System.out.println("Vähem kui kandeerimiseks vajalik.");
+        } else if (punktid >= 66 && punktid < 85) {
+            System.out.println("Kandideerimine vastuvõtule.");
+        } else if (punktid >= 85 && punktid <= 100) {
+            System.out.println("Vastuvõtt tagatud.");
+        } else if (punktid < 0 || punktid > 100) {
+            System.out.println("Vigane Tulemus.");
         }
     }
 }
