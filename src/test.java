74f328f703921dc8b64730d752df0ee475a100fc
Martin K <martin.kutt@khk.ee>
2021-01-04 14:15:39 +0200

    Kommenteerimine ja Väljastamine

--- src/test.java
+++ src/test.java
@@ -1,10 +1,16 @@
 public class test {
     public static void main(String[] args) {
+        /*
+        Kommenteerimine
+        Väljastamine
+        Katsetamine
+         */
         int rows = 5;
 
         for (int i = 1; i <= rows; ++i) {
             for (int j = 1; j <= i; ++j) {
                 System.out.print(j + " ");
+                //System.out.println(j+"Kurjam");
             }
             System.out.println();
         }
