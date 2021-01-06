98ff1cd3b546265fbc88c22c58b719b8f2941de0
Martin K <martin.kutt@khk.ee>
2021-01-06 09:38:44 +0200

    While loop ja ülesanne 3.2

--- src/test.java
+++ src/test.java
@@ -1,6 +1,6 @@
 /*
-Spämm
-ülesanne 2.3d
+Jänesevanemate mure 1
+ülesanne 3.2
  */
 
 import java.util.Scanner;
@@ -10,17 +10,24 @@ public class test {
         // Scanner
         Scanner sisend = new Scanner(System.in);
         // Kasutaja poolt sisend
-        System.out.println("Sisestage kirjasuurus: ");
-        float kirjaSuurus = sisend.nextFloat();
-        System.out.println("Sisestage kirja sisu: ");
-        String kirjaTeema = sisend.next();
-        System.out.println("Kas kirjal on fail: ");
-        Boolean kirjalFail = sisend.nextBoolean();
-        // kontroll
-        if (kirjaTeema == "" || kirjaSuurus > 1 && kirjalFail) {
-            System.out.println("Kiri on spämm.");
-        } else {
-            System.out.println("Kiri ei ole spämm.");
+        System.out.println("Sisestage ringidearv: ");
+        int ringideArv = sisend.nextInt();
+        int porganditeArv = 0;
+        int porganditeAndmine = 0;
+
+        int i = 0;
+        if (porganditeArv % 2 != 0) {
+            porganditeArv -= 1;
         }
+
+        while (i < ringideArv) {
+            i++;
+            if (i != porganditeAndmine) {
+                porganditeAndmine += 2;
+            } else {
+                porganditeArv += porganditeAndmine;
+            }
+        }
+        System.out.println(porganditeArv);
     }
 }
