7cda6bdbf0c7f655258c014255c21add61db3608
Martin K <martin.kutt@khk.ee>
2021-01-11 16:18:00 +0200

    Ülesanne 5.3 sissetulekud

--- src/test.java
+++ src/test.java
@@ -1,21 +1,32 @@
-/*
-Tervitused Mõtisklusega
-ülesanne 6.4a
- */
-
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
-        Scanner sisend = new Scanner(System.in);
 
-        System.out.println("Sisestage väärtus: ");
-        int a = sisend.nextInt();
+        ArrayList<Double> tehingud = new ArrayList<Double>();
 
-        for (int i = 0; i < a; i++) {
-            System.out.println("Kurjam");
+        try {
+            File myObj = new File("src/konto.txt");
+            Scanner sisendFail = new Scanner(myObj);
+            while (sisendFail.hasNextLine()) {
+                String rida = sisendFail.nextLine();
+                tehingud.add(Double.parseDouble(rida));
+                //System.out.println(rida);
+            }
+            sisendFail.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
         }
+
+        for (int i = 0; i < tehingud.size(); i++) {
+            if (tehingud.get(i) >= 0) {
+                System.out.println(tehingud.get(i));
+            }
+        }
+
     }
 }
