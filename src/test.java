aea4eb7728b01f23f41e3b7fc72b7fd1090252b2
Martin K <martin.kutt@khk.ee>
2021-01-11 16:27:28 +0200

    Ülesanne 5.4a Jukebox for, while ja lugemine failist

--- src/test.java
+++ src/test.java
@@ -1,3 +1,12 @@
+/*
+
+
+
+Ülesanne 5.4a
+Jukebox
+
+ */
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -6,15 +15,19 @@ import java.util.Scanner;
 public class test {
     public static void main(String[] args) {
 
-        ArrayList<Double> tehingud = new ArrayList<Double>();
+        Scanner sisend = new Scanner(System.in);
+
+        System.out.println("Sisestage faili nimi koos laiendiga: ");
+        String faili_nimi = sisend.nextLine();
+
+        ArrayList<String> laulud = new ArrayList<String>();
 
         try {
-            File file = new File("src/konto.txt");
+            File file = new File("src/" + faili_nimi);
             Scanner sisendFail = new Scanner(file);
             while (sisendFail.hasNextLine()) {
                 String rida = sisendFail.nextLine();
-                tehingud.add(Double.parseDouble(rida));
-                //System.out.println(rida);
+                laulud.add(rida);
             }
             sisendFail.close();
         } catch (FileNotFoundException e) {
@@ -22,11 +35,14 @@ public class test {
             e.printStackTrace();
         }
 
-        for (int i = 0; i < tehingud.size(); i++) {
-            if (tehingud.get(i) >= 0) {
-                System.out.println(tehingud.get(i));
-            }
+        for (int i = 0; i < laulud.size(); i++) {
+            System.out.println(i + ". " + laulud.get(i));
+
         }
+
+        System.out.println("Sisestage soovitud laulu järjekorranumber: ");
+        int laulu_number = sisend.nextInt();
+        System.out.println(laulud.get(laulu_number));
 
     }
 }
