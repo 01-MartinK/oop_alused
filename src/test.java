4452c40df3a9057e751473080ce060edad1429b9
Martin K <martin.kutt@khk.ee>
2021-01-13 13:49:33 +0200

    Ülesanne 5.4c Tahvli Juurde õpetaja juhendil

--- src/test.java
+++ src/test.java
@@ -1,14 +1,14 @@
 /*
 
-
-
-Ülesanne 5.4a
-Jukebox
+Ülesanne 5.4c
+Tahvli juurde
 
  */
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -20,14 +20,14 @@ public class test {
         System.out.println("Sisestage faili nimi koos laiendiga: ");
         String faili_nimi = sisend.nextLine();
 
-        ArrayList<String> laulud = new ArrayList<String>();
+        ArrayList<String> failiSisu = new ArrayList<String>();
 
         try {
             File file = new File("src/" + faili_nimi);
             Scanner sisendFail = new Scanner(file);
             while (sisendFail.hasNextLine()) {
                 String rida = sisendFail.nextLine();
-                laulud.add(rida);
+                failiSisu.add(rida);
             }
             sisendFail.close();
         } catch (FileNotFoundException e) {
@@ -35,14 +35,16 @@ public class test {
             e.printStackTrace();
         }
 
-        for (int i = 0; i < laulud.size(); i++) {
-            System.out.println(i + ". " + laulud.get(i));
-
+        LocalDateTime tänanekuupäev = LocalDateTime.now();
+        System.out.println(tänanekuupäev);
+        DateTimeFormatter tänasekuupäevaVormistus = DateTimeFormatter.ofPattern("dd");
+        String vormistatudKuupäev = tänanekuupäev.format(tänasekuupäevaVormistus);
+        System.out.println(vormistatudKuupäev);
+        /*
+        for (int i = 0;i < failiSisu.size();i++){
+            System.out.println(i+". "+failiSisu.get(i));
         }
-
-        System.out.println("Sisestage soovitud laulu järjekorranumber: ");
-        int laulu_number = sisend.nextInt();
-        System.out.println(laulud.get(laulu_number));
-
+         */
+        System.out.println("Tänane õpilane kes tuleb tahvlile on " + failiSisu.get(Integer.parseInt(vormistatudKuupäev)));
     }
 }
