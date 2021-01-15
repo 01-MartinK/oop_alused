39ee5a79a92577983be5bd31dd4441bab57c16c7
Martin K <martin.kutt@khk.ee>
2021-01-15 13:13:16 +0200

    Hashmap test

--- src/test.java
+++ src/test.java
@@ -1,7 +1,15 @@
 /*
 
-Ülesanne 6.4b
-Mündid
+Loo Hashmap tüüpi objekt main meetodis
+võti ja väärtus String
+salvesta mapi inimeste nimed - võtmena ja hüüdnimed - väärtusena
+kasuta andmete hoidmiseks ainult väikesed tähed
+
+matti - mage
+mikael - mixu
+arto - arppa
+
+Küsi kasutaja nimi ja trüki tema hüüdnimi
 
  */
 
@@ -10,24 +18,16 @@ import java.io.FileNotFoundException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class test {
-
-    public static int pronksiarvu_summa(ArrayList<Integer> a) {
-        int b = 0;
-        for (int i = 0; i < a.size(); i++) {
-            if (a.get(i) < 10) {
-                b = b + a.get(i);
-            }
-        }
-        return b;
-    }
 
     public static void main(String[] args) {
 
         Scanner sisend = new Scanner(System.in);
 
+        /*
         System.out.println("Sisestage faili nimi koos laiendiga: ");
         String faili_nimi = sisend.nextLine();
 
@@ -46,7 +46,27 @@ public class test {
             e.printStackTrace();
         }
 
-        System.out.println("Hoiupõrsasse läheb " + pronksiarvu_summa(failiSisu) + " senti.");
+         */
+
+        System.out.println("Sisestage nimi: ");
+        String nimi = sisend.nextLine();
+        nimi = nimi.toLowerCase();
+
+        HashMap<String, String> grupp = new HashMap<String, String>();
+
+        grupp.put("matti", "mage");
+        grupp.put("mikael", "mixu");
+        grupp.put("arto", "arppa");
+
+        System.out.println("Kasutaja: " + nimi + ", hüüdnimega " + grupp.get(nimi) + ".");
+
+        /*
+        for (String i : grupp.keySet()){
+            System.out.println("kasutaja: " + i + ", hüüdnimega " + grupp.get(i) + ".");
+            }
+        }
+
+         */
 
     }
 }
