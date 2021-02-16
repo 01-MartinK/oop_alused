c6840cebee215a0f42d5112fec9edfa440ea0101
Martin K <martin.kutt@khk.ee>
2021-02-16 12:34:27 +0200

    veel rohkem jappsi

--- src/Drawing.java
+++ src/Drawing.java
@@ -1,13 +1,22 @@
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
 import java.io.File;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 public class Drawing extends Canvas {
 
+    static Image image;
+
+    static String url = "src/maja.jpg";
+
+    //static BufferedImage img = ImageIO.read(url);
 
     public static void main(String[] args) {
+
+        image = Toolkit.getDefaultToolkit().getImage(url);
+
         JFrame frame = new JFrame("My Drawing");
         Canvas canvas = new Drawing();
         canvas.setSize(400, 400);
@@ -26,5 +35,12 @@ public class Drawing extends Canvas {
         g.fillRect(half.x, half.y, half.width, half.height);
         g.setColor(Color.BLACK);
         g.drawLine(0, 20, 100, 120);
+
+        g.drawImage(image, 15, 15, new ImageObserver() {
+            @Override
+            public boolean imageUpdate(Image img, int infoflags, int x, int y, int width, int height) {
+                return false;
+            }
+        });
     }
 }
